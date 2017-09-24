3fab81cc89b92f519082c887ff0482d1bd491ad1
zjw641220480 <2310700899@qq.com>
2017-09-24 16:47:19 +0800

    字符串相关函数初步完成

--- src/simple/str.py
+++ src/simple/str.py
@@ -7,12 +7,20 @@ Created on 2017年9月21日
 
 #去空格及特殊符号
 str='python String function';
-print len(str);#获取字符串的长度
-print str.upper();#字符串转大写
-print str.lower();#字符串转小写
-print str.swapcase();#字符串大小写互换
-print str.capitalize();#首字母大写，其余均小写
-print '%s lower=%s' % (str,str.lower())
-print str.find("t");#搜索第一次出现的下标
-print str.find("t",3);#从指定下标开始搜索，并且报错此下标
-print str.find("t",3,10);#从指定下标开始，截止到指定下标
+print (len(str));#获取字符串的长度
+print (str.upper());#字符串转大写
+print (str.lower());#字符串转小写
+print (str.swapcase());#字符串大小写互换
+print (str.capitalize());#首字母大写，其余均小写
+print ('%s lower=%s' % (str,str.lower()));
+print (str.find("t"));#搜索第一次出现的下标
+print (str.find("t",3));#从指定下标开始搜索，并且报错此下标
+print (str.find("t",3,10));#从指定下标开始，截止到指定下标
+print (str.rfind("t"));#从右边开始查找的符合的最后一个字符串位置；
+print (str.replace("t", "3"));#讲字符串中的所有老字符串替换为新的字符串
+print (str.strip());#去除字符串左右两边的空格
+print (str.split(" "));#按照空格进行切分，得到的是一个列表
+print (str.startswith("p"));
+print (str.isalnum());#判断是否全数字
+print (str.isalpha());#判断是否全字母
+print (int("345")+8);#字符串转化为int类型
